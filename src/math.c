7f531a5190fbe5d1c8c29f5c64b46009cb9d93b5
Rebecca 'TF' Nelson <tails.fanatic@gmail.com>
2015-05-04 09:30:29 -0500

    Added first files in math module

---
+++ src/math.c
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * This is implementation of the math module of the Hypatia AI system.
+ * Copyright (C) 2015  Rebecca Nelson
+ *
+ * To contact by email, send to username dekarrin on the domain outlook.com.
+ * To contact by mail, send to:
+ * Rebecca Nelson
+ * 1001 18th St N. Apt #8
+ * In the city of Fargo, in the state of North Dakota, with area code 58102
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have recieved a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "math.h"
